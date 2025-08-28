50bbf3778bc3aa00e05094b0f1467f248ab0ec43
Stiven-Richardy <stivenrichardy445@gmail.com>
2025-08-27 21:06:45 -0300

    Iniciando o projeto com as classes

---
+++ projeto-vendedores/Program.cs
@@ -0,0 +1,16 @@
+﻿/*
+| Instituto Federal de São Paulo - Campus Cubatão
+| Nome: Stiven Richardy Silva Rodrigues - CB3030202
+| Turma: ADS 471
+*/
+
+namespace projeto_vendedores
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+        }
+    }
+}
